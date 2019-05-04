aece95dd8e8755a69ba4979baa3d112b54c5742e
LUNKUS <sigitas@lunkus.com>
2019-05-04 13:44:28 +0300

    State design the obvious solution

---
+++ src/stateDesignPattern/MainState.java
@@ -0,0 +1,31 @@
+package stateDesignPattern;
+
+public class MainState {
+
+    public static void main(String[] args) {
+
+        SodaMachine sodaMachine = new SodaMachine(10);
+
+        sodaMachine.insertMoney();
+        sodaMachine.selectSoda();
+
+        System.out.println("Inventory: " + sodaMachine.count);
+        System.out.println("=======");
+
+        sodaMachine.insertMoney();
+        sodaMachine.ejectMoney();
+        sodaMachine.selectSoda();
+
+        System.out.println("Inventory: " + sodaMachine.count);
+        System.out.println("=======");
+
+        sodaMachine.insertMoney();
+        sodaMachine.selectSoda();
+        sodaMachine.insertMoney();
+        sodaMachine.selectSoda();
+        sodaMachine.ejectMoney();
+
+        System.out.println("Inventory: " + sodaMachine.count);
+        System.out.println("=======");
+    }
+}
