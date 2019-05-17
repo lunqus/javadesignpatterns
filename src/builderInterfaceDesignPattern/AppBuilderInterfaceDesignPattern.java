b14b1a5340012ccbb293054406c6d924334e109d
LUNKUS <sigitas@lunkus.com>
2019-05-17 10:27:30 +0300

    Builder design pattern done!

---
+++ src/builderInterfaceDesignPattern/AppBuilderInterfaceDesignPattern.java
@@ -0,0 +1,16 @@
+package builderInterfaceDesignPattern;
+
+public class AppBuilderInterfaceDesignPattern {
+
+    public static void main(String[] args) {
+
+        Person oldPerson = new Person.Builder("James", "Bond")
+                .phone("007")
+                .address("London")
+                .age(45)
+                .build();
+
+        System.out.println(oldPerson);
+
+    }
+}
